c4a8fba82069abc19a9f97f7f55825eaab8f9cc6
Michal Falat <michal.falat@zentity.com>
2021-11-28 22:09:11 +0100

    app module fix

--- edusys/apps/admin-management/src/app/app.module.ts
+++ edusys/apps/admin-management/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { CoreModule, httpInterceptorProviders } from '@edusys/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthFacade, CoreModule, httpInterceptorProviders } from '@edusys/core';
 import { CoreTranslateModule } from '@edusys/core-translate';
 import { APP_CONFIG } from '@edusys/app-config';
 import { LayoutModule } from './layouts/layout.module';
-import { MatDayjsDateModule } from '@tabuckner/material-dayjs-adapter';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { appConfig } from '../environments/environment';
+import { NgxPermissionsService } from 'ngx-permissions';
+import { MatDayjsDateModule } from '@tabuckner/material-dayjs-adapter';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { registerLocaleData } from '@angular/common';
+import localeSk from '@angular/common/locales/sk';
+import { AuthService } from 'libs/core/src/lib/services/auth/auth.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+registerLocaleData(localeSk);
+
+export const MY_DATE_FORMAT = {
+  display: {
+    dateInput: 'DD.MM. YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -26,7 +40,44 @@ import { appConfig } from '../environments/environment';
     MatDayjsDateModule,
     FlexLayoutModule,
   ],
-  providers: [{ provide: APP_CONFIG, useValue: appConfig }, httpInterceptorProviders],
+  providers: [
+    { provide: APP_CONFIG, useValue: appConfig },
+    httpInterceptorProviders,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadInitData,
+
+      deps: [HttpClient, AuthFacade, AuthService, NgxPermissionsService],
+      multi: true,
+    },
+    { provide: MAT_DATE_LOCALE, useValue: 'sk-SK' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMAT },
+    { provide: LOCALE_ID, useValue: 'sk' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function loadInitData(httpClient: HttpClient, authFacade: AuthFacade, authService: AuthService, ps: NgxPermissionsService): Function {
+  return () => {
+    const promise = new Promise<boolean>((resolve) => {
+      httpClient.get('assets/configuration.json').subscribe((res: APP_CONFIG) => {
+        if (appConfig.production) {
+          appConfig.apiUrl = res.apiUrl;
+          appConfig.appUrls = res.appUrls;
+        }
+        if (authService.getAuthToken()) {
+          authFacade.userInfo();
+          authFacade.fetchInitData((data) => {
+            ps.loadPermissions(data.permissions);
+            resolve(true);
+          });
+        } else {
+          resolve(true);
+        }
+      });
+    });
+
+    return promise;
+  };
+}
